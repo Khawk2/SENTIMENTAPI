75dc4fb5545e63d5ef09414f5f03a8cbe8970109
Javier Morales Labarca <j.morales.labarca@gmail.com>
2025-12-25 17:22:10 -0300

    se crea lógica de la clase service

--- src/main/java/com/example/demo/model/SentimentResponse.java
+++ src/main/java/com/example/demo/model/SentimentResponse.java
@@ -1,4 +1,8 @@
 package com.example.demo.model;
 
 public class SentimentResponse {
+    public SentimentResponse(String prevision, double probabilidad) {
+    }
+
+
 }
--- src/main/java/com/example/demo/service/SentimentService.java
+++ src/main/java/com/example/demo/service/SentimentService.java
@@ -1,4 +1,21 @@
 package com.example.demo.service;
 
+
+import com.example.demo.model.SentimentRequest;
+import com.example.demo.model.SentimentResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SentimentService {
+
+    public SentimentResponse analyze(SentimentRequest solicitud) {
+        String analisisTexto = solicitud.getText(); //se debe obtener solicitud del texto
+
+
+    //modelo de data science
+    String prevision = "Negativo";
+    double probabilidad = 9.91;
+
+    return new SentimentResponse (prevision, probabilidad);
+    }
 }
