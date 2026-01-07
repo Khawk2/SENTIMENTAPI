c4d1f7b6df967b20b22e52722c990330874e629e
Javier Morales Labarca <j.morales.labarca@gmail.com>
2026-01-07 10:29:20 -0300

    feat: endpoint GET /api/stats

--- src/main/java/com/example/demo/controller/SentimentController.java
+++ src/main/java/com/example/demo/controller/SentimentController.java
@@ -30,34 +30,13 @@ public class SentimentController {
 
         return ResponseEntity.ok(response);
 
+    @GetMapping("/status")
+            public ResponseEntity<Map<String, String>> verStatus; {
+        ResponseEntity.ok(Map.of(
+                "Estado", "OK",
+                "Servicio", "SentimentApi",
+                "Base de Datos", "Conectada"
+        ));
+        }
 
-import com.example.demo.model.SentimentRequest;
-import com.example.demo.model.SentimentResponse;
-import com.example.demo.service.SentimentService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping ("/sentiment")
-
-public class SentimentController {
-
-    private final SentimentService servicioSentimiento;
-
-    public SentimentController (SentimentService servicioSentimiento){
-        this.servicioSentimiento = servicioSentimiento;
-            }
-
-    @PostMapping
-    public ResponseEntity<SentimentResponse> analyzeSentiment(@Validated @RequestBody SentimentRequest solicitud){
-        SentimentResponse respuesta = servicioSentimiento.analyze(solicitud);
-
-        return ResponseEntity.ok(respuesta);
-
-
-    }
 }
