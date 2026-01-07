d2f29feaf8651df147c9e9386f84119fa775363a
Javier Morales Labarca <j.morales.labarca@gmail.com>
2026-01-06 21:51:43 -0300

    feat: se agrega validación que solo acepte alfanuméricos y no solo números en el textbox

--- src/main/java/com/example/demo/dto/SentimentRequest.java
+++ src/main/java/com/example/demo/dto/SentimentRequest.java
@@ -3,13 +3,18 @@ package com.example.demo.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.annotations.processing.Pattern;
 
 @Data
 public class SentimentRequest {
 
     @NotBlank(message = "El campo 'text' es obligatorio")
-    @Size(min = 1, message = "El texto debe tener al menos 1 carácter")
-    private String text;
+    @Size(min = 3, message = "El texto debe tener al menos 3 carácter")
 
+    @Pattern(
+            regexp = "^(?=.*[a-zA-ZáéíóúÁÉÍÓÚñÑ])[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\\s.,!?]+$",
+            message = "El texto debe contener letras y no puede ser únicamente numérico"
+    )
 
+private String text;
 }
